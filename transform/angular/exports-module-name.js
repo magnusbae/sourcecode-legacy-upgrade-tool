a547c439939f40cf48eeb1ae27237b08c890da22
Magnus Bae <magnusbae@gmail.com>
2016-12-09 14:41:06 +0100

    WIP building objects

--- transform/angular/exports-module-name.js
+++ transform/angular/exports-module-name.js
@@ -2,14 +2,41 @@ function transformer(file, api) {
   const j = api.jscodeshift;
   const {expression, statement, statements} = j.template;
 
+  const root = j(file.source);
 
-  let varname = j(file.source)
-    .find(j.Identifier, id => id.name === 'angular')
-    .filter(angular => angular.parent.getValueProperty('property').name === 'module')
-    .get().parent.parent.getValueProperty('arguments')[0].value;
-  return j(file.source)
-      .toSource()
-    + "module.exports = " + varname + ";";
+  // let varname = root
+  //   .find(j.Identifier, id => id.name === 'angular')
+  //   .filter(angular => angular.parent.getValueProperty('property').name === 'module')
+  //   .get().parent.parent.getValueProperty('arguments')[0].value;
+
+
+  /*
+  *
+   def("VariableDeclaration")
+   .bases("Declaration")
+   .build("kind", "declarations")
+   .field("kind", or("var", "let", "const"))
+   .field("declarations", [def("VariableDeclarator")]);
+
+   def("VariableDeclarator")
+   .bases("Node")
+   .build("id", "init")
+   .field("id", def("Pattern"))
+   .field("init", or(def("Expression"), null));
+  * */
+
+  return(j.variableDeclaration([
+    'var',
+    [j.variableDeclarator([
+      'foo',
+      j.literal('bar')
+    ])]
+  ])).toSource();
+
+
+  // return j(file.source)
+  //     .toSource()
+  //   + "module.exports = " + varname + ";";
 }
 
 module.exports = transformer;
